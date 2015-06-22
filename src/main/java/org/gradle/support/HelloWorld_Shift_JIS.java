48f63e0648c20220dd22ababe24cc4f78d673b48
iluxa-com <iluxa_com@mail.ru>
2015-06-22 18:44:39 +0300

    jap->Shift-JIS

--- src/main/java/org/gradle/support/HelloWorld_Shift_JIS.java
+++ src/main/java/org/gradle/support/HelloWorld_Shift_JIS.java
@@ -3,7 +3,7 @@ package org.gradle.support;
 
 import java.util.function.Supplier;
 
-public class HelloWorld_jap1 {
+public class HelloWorld_Shift_JIS {
 	//ŒN‚ª‘ã
 	public static void main(String[] args) {
 		Supplier<String> /*ŒN‚ª‘ã*/helloer = () -> "Hello World!!!!" ;
