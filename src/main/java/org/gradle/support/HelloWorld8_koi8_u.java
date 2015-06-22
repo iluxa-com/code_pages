0d8bd6797df6ca11d3d990961b366a95846cf163
iluxa-com <iluxa_com@mail.ru>
2015-06-22 14:10:04 +0300

    +cp

---
+++ src/main/java/org/gradle/support/HelloWorld8_koi8_u.java
@@ -0,0 +1,13 @@
+//русский кои-8
+package org.gradle.support;
+
+import java.util.function.Supplier;
+
+public class HelloWorld8_koi8_u {
+	
+	public static void main(String[] args) {
+		Supplier<String> /*русский кои-8*/helloer = () -> "Hello World!!!!" ;
+		System.out.println(helloer.get());
+	}
+}
+//comment
